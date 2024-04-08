9a3c0a9a276b7ef944a0cd8816554e4888ab38db
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-04-08 13:51:47 +0530

    Update settings.json, SettingsModal.tsx, ConversationList.tsx, layout.tsx, and useRoutes.ts

--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -1,13 +1,21 @@
-import { useMemo } from "react";
+import { useState, useMemo } from "react";
 import { usePathname } from "next/navigation";
 import { HiChat } from 'react-icons/hi';
 import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2';
 import { signOut } from "next-auth/react";
 import useConversation from "./useConversation";
+import Loading from '../conversations/loading'; // import your loading component
 
 const useRoutes = () => {
   const pathname = usePathname();
   const { conversationId } = useConversation();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignOut = async () => {
+    setIsLoading(true);
+    await signOut();
+    setIsLoading(false);
+  };
 
   const routes = useMemo(() => [
     { 
@@ -16,19 +24,19 @@ const useRoutes = () => {
       icon: HiChat,
       active: pathname === '/conversations' || !!conversationId
     },
-    { 
-      label: 'Users', 
-      href: '/users', 
-      icon: HiUsers, 
-      active: pathname === '/users'
-    },
+    // { 
+    //   label: 'Users', 
+    //   href: '/users', 
+    //   icon: HiUsers, 
+    //   active: pathname === '/users'
+    // },
     {
-      label: 'Logout', 
-      onClick: () => signOut(),
+      label: isLoading ? 'Loading...' : 'Logout', 
+      onClick: handleSignOut,
       href: '#',
-      icon: HiArrowLeftOnRectangle, 
+      icon: isLoading ? Loading : HiArrowLeftOnRectangle, 
     }
-  ], [pathname, conversationId]);
+  ], [pathname, conversationId, isLoading]);
 
   return routes;
 };
